e912abf846d7034ac96fb78358992e8b56da10b6
yasinguzel <yasin.guzel@useinsider.com>
2019-02-24 05:03:29 +0300

    add fun for crate random missing data frame

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from src.utils import *
 
-k = input('Enter value of k = ')
+# k = input('Enter value of k = ')
 
 dataset = read_csv_file()
 
@@ -13,3 +13,9 @@ train_data_length = calculate_data_length(TRAIN_DATA_RATIO, DATASET_LENGTH, RATI
 train_data = dataset[:train_data_length]
 
 query_data = dataset[train_data_length:]
+
+save_data_frame_as_a_csv(query_data, 'query_data')
+
+missing_data = delete_cell_from_row_random(query_data)
+
+save_data_frame_as_a_csv(missing_data, 'missing_data')
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import random
 
 
 def read_csv_file():
@@ -8,3 +9,15 @@ def read_csv_file():
 
 def calculate_data_length(data_ratio, dataset_length, ratio):
     return int(data_ratio * dataset_length / ratio)
+
+
+def save_data_frame_as_a_csv(data_frame, name):
+    data_frame.to_csv('./results/{name}.csv'.format(name=name), sep=';')
+
+
+def delete_cell_from_row_random(query_data):
+    for index, row in query_data.iterrows():
+        random_column = random.choice(list(query_data.columns.values))
+
+        query_data.at[index, random_column] = 111111111111111111
+    return query_data
