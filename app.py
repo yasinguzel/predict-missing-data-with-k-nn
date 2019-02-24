694b147608b9dd92bc355395883ff92dd20761c0
yasinguzel <yasin.guzel@useinsider.com>
2019-02-24 17:45:40 +0300

    add normalization step

--- app.py
+++ app.py
@@ -4,18 +4,18 @@ from src.utils import *
 
 dataset = read_csv_file()
 
+dataset = normalize_dataset(dataset)
+save_data_frame_as_a_csv(dataset, 'normalized_data')
+
 RATIO = 4
 TRAIN_DATA_RATIO = 3
 DATASET_LENGTH = len(dataset.index)
 
 train_data_length = calculate_data_length(TRAIN_DATA_RATIO, DATASET_LENGTH, RATIO)
-
 train_data = dataset[:train_data_length]
 
 query_data = dataset[train_data_length:]
-
 save_data_frame_as_a_csv(query_data, 'query_data')
 
 missing_data = delete_cell_from_row_random(query_data)
-
 save_data_frame_as_a_csv(missing_data, 'missing_data')
--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,20 @@
 import pandas as pd
 import random
+from sklearn import preprocessing
+import numpy
 
 
 def read_csv_file():
     data = pd.read_csv('./dataset/ENB2012_data.csv', delimiter=';')
     return data
+
+
+def normalize_dataset(dataset):
+    array = dataset.values
+    min_max_scalar = preprocessing.MinMaxScaler()
+    scaled_array = min_max_scalar.fit_transform(array)
+    dataset = pd.DataFrame(scaled_array)
+    return dataset
 
 
 def calculate_data_length(data_ratio, dataset_length, ratio):
@@ -19,5 +29,6 @@ def delete_cell_from_row_random(query_data):
     for index, row in query_data.iterrows():
         random_column = random.choice(list(query_data.columns.values))
 
-        query_data.at[index, random_column] = 111111111111111111
+        query_data.at[index, random_column] = numpy.nan
+    print(query_data)
     return query_data
