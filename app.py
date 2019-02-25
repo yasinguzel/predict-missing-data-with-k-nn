4aa0799492792a94465a114e779b8869d3533750
yasinguzel <yasin.guzel@useinsider.com>
2019-02-25 18:04:52 +0300

    sort distances

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ missing_data = delete_cell_from_row_random(query_data)
 save_data_frame_as_a_csv(missing_data, 'missing_data')
 
 distances_list = [[]]
+distance_df = pd.DataFrame()
 
 for missing_data_row, missing_data_column in zip(*numpy.where(pd.isnull(missing_data))):
 
@@ -34,11 +35,16 @@ for missing_data_row, missing_data_column in zip(*numpy.where(pd.isnull(missing_
 
     for train_data_index, train_data_row in copy_train_data.iterrows():
         sum_one_point_distance = 0
+
         for train_data_cell, missing_data_cell in zip(train_data_row, copy_missing_data.iloc[missing_data_row]):
             one_point_distance = pow((train_data_cell - missing_data_cell), 2)
             sum_one_point_distance += one_point_distance
+
         distance = math.sqrt(sum_one_point_distance)
         distances_list.append([train_data_index, distance])
 
-distance_data_frame = pd.DataFrame(distances_list, columns=['row', 'distance'])
-save_data_frame_as_a_csv(distance_data_frame, 'distance')
+    distance_df = pd.DataFrame(distances_list, columns=['row', 'distance'])
+    sorted_distance_df = distance_df.sort_values('distance')
+    print(sorted_distance_df, '\n')
+    distance_df.drop(distance_df.columns, axis=1)
+    distances_list = [[]]
